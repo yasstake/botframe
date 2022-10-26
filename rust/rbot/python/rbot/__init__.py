2ecc2715ffb0a83206411c19d8d68da0572546d1
yass <yass@takibi.net>
2022-10-26 12:42:35 +0900

    implemnt select method with closure

---
+++ rust/rbot/python/rbot/__init__.py
@@ -0,0 +1,6 @@
+from .rbot import *
+
+
+__doc__ = rbot.__doc__
+if hasattr(rbot, "__all__"):
+	__all__ = rbot.__all__
--- rust/rbot/src/db/sqlite.rs
+++ rust/rbot/src/db/sqlite.rs
@@ -1,12 +1,78 @@
 use std::sync::mpsc;
 use std::sync::mpsc::{Receiver, Sender};
 use std::thread;
-use rusqlite::{params, Connection, Result, Statement, Transaction, Params, Error, MappedRows, Row, params_from_iter};
+use rusqlite::{params, Connection, Result, Statement, Transaction, Params, Error, MappedRows, Row, Rows, params_from_iter};
 use crate::common::order::Trade;
-use crate::common::time::MicroSec;
+use crate::common::time::{MicroSec, self};
 use crate::OrderSide;
 
+#[derive(Debug)]
+pub struct Ohlcvv {
+    pub time: f64,
+    pub open: f64,
+    pub high: f64,
+    pub low: f64,
+    pub close: f64,
+    pub vol: f64,
+    pub sell_vol: f64,
+    pub sell_count: f64,
+    pub buy_vol: f64,
+    pub buy_count: f64,
+    pub start_time: f64,
+    pub end_time: f64,
+}
 
+impl Ohlcvv {
+    pub fn new() -> Self {
+        Ohlcvv { 
+            time: 0.0, 
+            open: 0.0, 
+            high: 0.0, 
+            low: 0.0, 
+            close: 0.0, 
+            vol: 0.0, 
+            sell_vol: 0.0,
+            sell_count: 0.0, 
+            buy_vol: 0.0,
+            buy_count: 0.0,
+            start_time: 0.0,
+            end_time: 0.0 
+        }
+    }
+
+    pub fn append(&mut self, trade: &Trade) {
+        if self.start_time == 0.0 || (trade.time as f64) < self.start_time {
+            self.start_time = trade.time as f64;
+            self.open = trade.price;
+        }
+
+        if self.end_time == 0.0 || self.end_time < trade.time as f64 {
+            self.end_time = trade.time as f64;
+            self.close = trade.price;
+        }
+
+        if self.high < trade.price {
+            self.high = trade.price;
+        }
+
+        if trade.price < self.low || self.low == 0.0 {
+            self.low = trade.price;
+        }
+
+
+        self.vol += trade.size;
+
+        if  trade.order_side == OrderSide::Sell {
+            self.sell_vol += trade.size;
+            self.sell_count += 1.0;
+        }
+        else if trade.order_side == OrderSide::Buy {
+            self.buy_vol += trade.size;
+            self.buy_count += 1.0;
+        }
+
+    }
+}
 
 pub struct TradeTable {
     connection: Connection,
@@ -101,6 +167,146 @@ impl TradeTable {
         }
     }
 
+    pub fn select_ohlcvv(&mut self, from_time: MicroSec, to_time: MicroSec, windows_sec: i64) -> Vec<Ohlcvv> {
+        let mut sql = "";
+        let mut param= vec![];
+
+        if 0 < to_time {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp and time_stamp < $2";
+            param = vec![from_time, to_time];
+        }
+        else {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp ";
+            param = vec![from_time];
+        }
+
+        let mut statement = self.connection.prepare(sql).unwrap();
+
+        let time_iters= statement.query_map(params_from_iter(param.iter()), |row| {
+            let bs_str: String = row.get_unwrap(1);
+            let bs = OrderSide::from_str(bs_str.as_str());
+
+            Ok(Trade {
+                time: row.get_unwrap(0),
+                price: row.get_unwrap(2),
+                size: row.get_unwrap(3),
+                order_side: bs,
+                liquid: row.get_unwrap(4),
+                id: row.get_unwrap(5)
+            })
+        }).unwrap();
+
+        let mut ohlcvv: Vec<Ohlcvv> = vec![];
+        let mut chunk: Ohlcvv = Ohlcvv::new();
+
+        // TODO: implement time window
+        for trade in time_iters {
+            if chunk.time == 0.0 {
+                
+                
+            }
+
+            let t = trade.unwrap();
+            chunk.append(&t);
+            // chunk = Ohlcvv::new();
+        }
+
+        ohlcvv.push(chunk);
+        return ohlcvv;
+
+    }
+
+    fn conv(rows: &mut Rows) -> Trade {
+        let row = rows.next().unwrap().unwrap();
+        let bs_str: String = row.get_unwrap(1);
+        let bs = OrderSide::from_str(bs_str.as_str());
+        Trade {
+            time: row.get_unwrap(0),
+            price: row.get_unwrap(2),
+            size: row.get_unwrap(3),
+            order_side: bs,
+            liquid: row.get_unwrap(4),
+            id: row.get_unwrap(5)
+        }
+    }
+
+
+    pub fn select<F>(&mut self, f: F, from_time: MicroSec, to_time: MicroSec) where F: Fn(&Trade) {
+        let mut sql = "";
+        let mut param= vec![];
+
+        if 0 < to_time {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp and time_stamp < $2";
+            param = vec![from_time, to_time];
+        }
+        else {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp ";
+            param = vec![from_time];
+        }
+
+        let mut statement = self.connection.prepare(sql).unwrap();        
+
+        let _transaction_iter = statement.query_map(params_from_iter(param.iter()), |row| {
+
+            let bs_str: String = row.get_unwrap(1);
+            let bs = OrderSide::from_str(bs_str.as_str());
+
+            Ok(Trade {
+                time: row.get_unwrap(0),
+                price: row.get_unwrap(2),
+                size: row.get_unwrap(3),
+                order_side: bs,
+                liquid: row.get_unwrap(4),
+                id: row.get_unwrap(5)
+            })
+        }).unwrap();
+
+        for trade in _transaction_iter {
+            match trade {
+                Ok(t) => f(&t),
+                Err(e) => log::error!("{:?}", e)
+            }
+        }
+
+        /*
+
+        let mut statement = self.connection.prepare(sql).unwrap();
+
+        // let time_iters= statement.query_map(params_from_iter(param.iter()), conv);
+        //return Box::new(statement.query(params_from_iter(param.iter())));
+        let iters = statement.query(params_from_iter(param.iter()));
+
+        for rows in iters {
+            f(rows);
+        }
+        */
+    }
+
+
+    /*
+    pub fn select(&mut self, from_time: MicroSec, to_time: MicroSec) -> Rows {
+        let mut sql = "";
+        let mut param= vec![];
+
+        if 0 < to_time {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp and time_stamp < $2";
+            param = vec![from_time, to_time];
+        }
+        else {
+            sql = "select time_stamp, action, price, size, liquid, id from trades where $1 <= time_stamp ";
+            param = vec![from_time];
+        }
+
+        let mut statement = self.connection.prepare(sql).unwrap();
+
+        // let time_iters= statement.query_map(params_from_iter(param.iter()), conv);
+        //return Box::new(statement.query(params_from_iter(param.iter())));
+        let iters = statement.query(params_from_iter(param.iter())).unwrap();
+
+        return iters;
+    }
+    */
+
     pub fn insert_records(&mut self, trades: Vec<Trade>) {
         let mut statement = self.connection.prepare(
             r#"insert or replace into trades (time_stamp, action, price, size, liquid, id)
@@ -131,6 +337,8 @@ mod test_transaction_table {
     use std::sync::mpsc::{Receiver, Sender};
     use std::thread;
     use chrono::Duration;
+    use crate::fs::db_full_path;
+
     use super::*;
 
     #[test]
@@ -171,6 +379,29 @@ mod test_transaction_table {
         table.select_time(2, 0);
         println!("1-3");
         table.select_time(1, 3);
+    }
+
+    #[test]
+    fn test_select_fn() {
+        test_insert_table();
+
+        let mut table = TradeTable::open("test.db").unwrap();
+        println!("0-0");
+
+        table.select(|row|{println!("{:?}",row)}, 0, 0);
+    }
+
+
+
+    #[test]
+    fn test_select_ohlcv() {
+        let db_name = db_full_path("FTX", "BTC-PERP");
+
+        let mut db = TradeTable::open(db_name.to_str().unwrap()).unwrap();
+
+        let ohlcv = db.select_ohlcvv(0, 0, 120);
+
+        println!("{:?}", ohlcv);
     }
 
     #[test]
